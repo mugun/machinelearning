21aacaaf4bcdf44032f4c22c37d90ccfae284314
mugun <fadefatemugun>
2018-09-26 20:47:11 +0800

    更新了贝叶斯部分的代码，但没有更新样例实际应用的代码

--- naive bayes/bayes.py
+++ naive bayes/bayes.py
@@ -30,16 +30,19 @@ def setOfWords2Vec(vocabList,inputSet):
             returnVec[vocabList.index(word)]=1
         else:
             print(word," not in the vocabulary")
+    #print(returnVec)
     return returnVec
 
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs=len(trainMatrix)
     numWords=len(trainMatrix[0])
     pAbusive=sum(trainCategory)/float(numTrainDocs)
-    p0Num=np.zeros(numWords)
-    p1Num=np.zeros(numWords)
-    p0Denom=0.0
-    p1Denom=0.0
+    print(sum(trainCategory)/float(numTrainDocs))
+    print(trainCategory)
+    p0Num=np.ones(numWords)
+    p1Num=np.ones(numWords)
+    p0Denom=2.0
+    p1Denom=2.0
     for i in range(numTrainDocs):
         if trainCategory[i]==1:
             #进行向量相加
@@ -48,11 +51,41 @@ def trainNB0(trainMatrix,trainCategory):
         else:
             p0Num+=trainMatrix[i]
             p0Denom+=sum(trainMatrix[i])
-    p1Vect=p1Num/p1Denom
+    p1Vect=np.log(p1Num/p1Denom)
+    #这里使用的log方法应该是numpy中的而不是math中的
     #对每个元素做除法
-    p0Vect=p0Num/p0Denom
+    p0Vect=np.log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive
             
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+np.log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+np.log(1.0-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts,listClasses=loadDataSet()
+    myVocabList=createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb=trainNB0(np.array(trainMat),np.array(listClasses))
+    testEntry=['love','my','dalmation']
+    thisDoc=np.array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'calssified as:',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry=['stupid','garbage']
+    thisDoc=np.array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,'calssified as:',classifyNB(thisDoc,p0V,p1V,pAb))
+
+def bagOfWords2VecMN(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]+=1
+    return returnVec
 
 if __name__=='__main__':
     listOPost,listClasses=loadDataSet()
@@ -64,3 +97,4 @@ if __name__=='__main__':
         trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
     p0V,p1V,pAb=trainNB0(trainMat,listClasses)
     print(pAb)
+    #testingNB()
